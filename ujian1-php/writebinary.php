1d45c16b62030086061221b24f42e04d837079d2
azharisalleh <azhari.salleh@gmail.com>
2015-10-10 19:04:38 +0800

    Update read and write binary
    Update php file for read and write

--- ujian1-php/writebinary.php
+++ ujian1-php/writebinary.php
@@ -1,7 +1,7 @@
 <?php
 //Created by azhari hj salleh
 //ID 2014461166
-//This program for write content to azhari.txt
+//This program for write content to azhari.bin
 
 /*     Name:
        Matrix No:
@@ -11,15 +11,28 @@
        Mobile Phone:
 */
 
-$fp = fopen("binary-azhari.dat", "wb");
-for ($i = 0; $i < 256; $i++) {
-    // Assign a binary byte to a variable
-    $data = pack("C*",$i);
- 
-    // Write the byte to the file
-    fwrite($fp, $data);
-}
-fclose($fp);
+$h=fopen("azhari.bin", 'w+');
+
+if(fwrite($h,'Name : Azhari Hj Salleh'."\n") == false)
+echo 'Error Writing';
+
+if(fwrite($h,'Matrix No : 2014461166'."\n") == false)
+echo 'Error Writing';
+
+if(fwrite($h,'Gender : Male'."\n") == false)
+echo 'Error Writing';
+
+if(fwrite($h,'Age : 40'."\n") == false)
+echo 'Error Writing';
+
+if(fwrite($h,'Email : azhari.salleh@gmail.com'."\n") == false)
+echo 'Error Writing';
+
+if(fwrite($h,'Mobile No : 013 66666666'."\n") == false)
+echo 'Error Writing';
+
+fclose($h);
+echo 'Written Done.';
 
 
 ?>
